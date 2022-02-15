771b67b484d44e93cc0155ee6f975c2113c0da65
Jonathan Barnum <jonbarnum07@gmail.com>
2022-02-15 11:44:52 -0700

    initial files for bounty hunter

---
+++ bounty_hunter/bountyRouter.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bountyRouter = express.Router()
+const {v4: uuidv4} = require('uuid')
+
+
+const bounties = [
+    {
+        firstName: 'Crazy',
+        lastName: 'Killer',
+        living: true,
+        bountyAmount: 100,
+        type: 'Sith',
+        _id: uuidv4()
+    }
+]
+
+
+bountyRouter.get('/', (req, res) => {
+    res.send(bounties)
+})
+
+bountyRouter.post('/', (req, res) => {
+    const newBounty = req.body
+    newBounty._id = uuidv4()
+    bounties.push(newBounty)
+    res.send(`You have a new bounty ${newBounty.firstName} ${newBounty.lastName}. They are a ${newBounty.type} with an award amount of $${newBounty.bountyAmount}`)
+})
+
+
+module.exports = bountyRouter
